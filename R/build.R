11a9342280e5f482a8d53dea9f8278385fb39831
Tuomisto Jouni <jouni.tuomisto@thl.fi>
2019-05-07 17:13:37 +0300

    Corrections to qload and qbuild by Juha Yrjölä

--- R/build.R
+++ R/build.R
@@ -9,40 +9,10 @@
 #' @examples
 #' data(iris)
 #' qbuild("hua/iris", iris = iris)
+
 qbuild <- function(pkg, ...) {
-    quilt <- reticulate::import("quilt")
-    pyfeather <- reticulate::import(module = "feather")
-
-    pkg_split <- stringr::str_split(pkg, "/")[[1]]
-
-    store <- reticulate::import("quilt.tools.store")
-    pkg_obj <- store$PackageStore$find_package(pkg_split[1], pkg_split[2])
-    # if(!is.null(pkg_obj)) {
-        # pkg_path <- pkg_obj.get_path()
-    # } else {
-        # pkg_path <- tempdir()
-        # pkg_path <- paste0(getwd(), "/quilt_packages")
-    # }
-
-    # create temp dir to build the package with
-    pkg_path <- paste0(tempdir(), "/", pkg_split[2])
-    if(!dir.exists(pkg_path))
-        dir.create(pkg_path)
-
-    # write each file to the temp dir
-    files <- list(...)
-    for(i in 1:length(files)) {
-        file_name <- names(files)[i]
-        file_path <- paste0(pkg_path, "/", file_name)
-        if(tolower(tools::file_ext(file_name)) != "") {
-            writeLines(text = paste(files[i]), con = file_path)
-        } else {
-            write.csv(files[i], file = file_path)
-        }
-    }
-    quilt$build(pkg, pkg_path)
-
-    # unlink(pkg_path, recursive = TRUE)
+  quilt <- reticulate::import("quilt")
+  quilt$build(pkg, ...)
 }
 
 #' Push package from local to remote
--- R/io.R
+++ R/io.R
@@ -13,20 +13,11 @@
 #' qload("examples/wine", "quality/red")
 #' qload("akarve/seattle_911", "responses")
 qload <- function(pkg, file) {
-    info_df <- qparse(pkg, file)
-
-    if(paste(info_df$class) != "TableNode") {
-        stop("Not a TableNode")
-    }
-
-    pkg_pythonic <- stringr::str_replace_all(pkg, "/", "\\.")
-    pkg_name <- paste0("quilt.data.", pkg_pythonic)
-    data <- reticulate::import(module = pkg_name)
-    file <- stringr::str_replace_all(file, "/", "$")
-
-    df <- eval(parse(text = paste0("data$", file, "()")))
-    tmp <- tempfile(pattern = "quilt", fileext = "feather")
-    pyfeather <- reticulate::import(module = "feather")
-    pyfeather$write_dataframe(df$copy(), tmp)
-    feather::read_feather(tmp)
+  pkg_pythonic <- stringr::str_replace_all(pkg, "/", "\\.")
+  pkg_name <- paste0("quilt.data.", pkg_pythonic)
+  data <- reticulate::import(module = pkg_name)
+  file <- stringr::str_replace_all(file, "/", "$")
+  
+  df <- eval(parse(text = paste0("data$", file, "()")))
+  df
 }
